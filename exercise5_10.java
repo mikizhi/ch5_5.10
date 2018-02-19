0c1a9fe982da4f8164a2eb52ac6bbbdbc131131a
mikizhi <mikizhi@gmail.com>
2018-02-19 16:13:02 -0500

    Add files via upload

---
+++ exercise5_10.java
@@ -0,0 +1,24 @@
+public class ch5_5dot10 {
+
+	public static void main(String[] args) {
+		
+		int count = 0;
+		
+		for (int i=100; i<=1000; i++){
+			if(count < 10)
+				if(i%5==0 && i%6 ==0){
+					System.out.print(i+" ");
+					count++;
+				}
+			
+			if(count ==10)
+			{
+				System.out.println();
+				count = 0;
+			}
+		}
+		
+
+	}
+
+}
